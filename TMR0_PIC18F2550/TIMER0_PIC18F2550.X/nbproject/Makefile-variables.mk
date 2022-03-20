7f6d7eedadc4f39cff57174f0c6668942a691c79
Carlos Quispe <carlos359_7@hotmail.com>
2022-03-19 19:58:50 -0500

    Add files via upload

---
+++ TMR0_PIC18F2550/TIMER0_PIC18F2550.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TIMER0_PIC18F2550.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TIMER0_PIC18F2550.X.production.hex
